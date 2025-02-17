4c6629a8981c5f5bf78c41d1c966db0ca40bde96
cansahinbas <jshahimbash@gmail.com>
2025-02-17 16:28:28 +0400

    second commit

---
+++ day 02/homework/davaleba3.py
@@ -0,0 +1,7 @@
+lenght=(10)
+
+width=(20)
+
+print(lenght+width+lenght+width)
+
+#მართკუთხედის პერიმეტრია:60 სმ.
---
+++ day 02/homework/jan.py
@@ -0,0 +1,11 @@
+number=(5)
+
+number1=(8)
+
+number2=(3)
+
+number3=(9)
+
+number4=(10)
+
+print(number+number1+number2+number3+number4)
