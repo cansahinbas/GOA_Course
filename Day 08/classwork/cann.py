f0643031e7690556dfacd82d9a008a507640cb6a
cansahinbas <jshahimbash@gmail.com>
2025-03-05 20:37:16 +0400

    second commit

--- Day 08/classwork/cann.py
+++ Day 08/classwork/cann.py
@@ -1,3 +1,22 @@
-print("Enter your name:")
-x = input("ჯან")
-print("Hello, " + x)
+#______________________________
+
+#Name = "Nika"
+#naMe = "Luka"
+#NAME = "Data"
+#naME = "Vano"
+#name = "Demetre"
+#NaMe = "Jan"
+#name = "Giorgi"
+#აქ შეიქმნება 6 ცვლადი
+
+#__________________________________
+
+#________________________________________________
+
+can_sahinbas="14"
+can_Sahinbas="15"
+Can_sahinbas="16"
+caN_sahinbas="17"
+can_saHinbas="18"
+
+print(can_sahinbas,can_Sahinbas,Can_sahinbas,caN_sahinbas,can_saHinbas)
