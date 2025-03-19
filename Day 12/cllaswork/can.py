3a3a83b837fb85261d2d61174cb07a5d81e053ce
cansahinbas <jshahimbash@gmail.com>
2025-03-19 19:19:59 +0400

    second commit

---
+++ Day 12/cllaswork/can.py
@@ -0,0 +1,11 @@
+number=input("enter  number: ")
+
+print(type(number))
+
+number=int(input("enter  number: "))
+
+print(type(number))
+
+number=float(input("enter  number: "))
+
+print()
---
+++ Day 12/homework/can.py
@@ -0,0 +1,14 @@
+number="12"
+
+number1=(6)
+
+print(number1+int(number))
+
+print(number1-int(number))
+
+print(number1*int(number))
+
+print(number1/int(number))
+
+
+
