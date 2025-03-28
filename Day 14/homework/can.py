d14580d2ba71c2be1ad017dd34966f244cd9265d
cansahinbas <jshahimbash@gmail.com>
2025-03-28 17:36:13 +0400

    second commit

---
+++ Day 14/homework/can.py
@@ -0,0 +1,10 @@
+register_email=input("email: ")
+register_pasword=input("pasword: ")
+
+login_email=input("email: ")
+login_pasword=input("pasword: ")
+
+print (register_email==login_email)
+
+print(register_pasword==login_pasword)
+
---
+++ Day 14/homework/can1.py
@@ -0,0 +1,11 @@
+print(True and True)   
+
+#True
+
+print(True and False)  
+ 
+#False
+
+print(15 > 3 and 21 < 202)  
+
+#True
