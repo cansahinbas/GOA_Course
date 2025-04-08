911e8aee50dffbabab175fc89403fb3aa25b0591
cansahinbas <jshahimbash@gmail.com>
2025-04-08 15:10:15 +0400

    second commit

---
+++ Day 16/homework/can.py
@@ -0,0 +1,21 @@
+#Sequence არის ელემენტების მოწყობილი რიგი სადაც თითოეულ ელემენტს თავისი ადგილი აქვს
+#Selection გულისხმობს გადაწყვეტილების მიღებას გარკვეული პირობის მიხედვით
+
+name=input("enter your name: ")
+
+if name == "can":
+    print("your name can")
+else:
+    print("your name is not can")
+
+age=input("enter your age")
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+
+    
+
+
---
+++ Day 16/homework/can1.py
@@ -0,0 +1,12 @@
+name="can"
+
+input=input("enter pasword: ")
+
+if input == 1234:
+    print("Password is correct")
+else:
+    print("Password is incorrect")
+
+
+
+
