4b31da799be14c09e2d0d691f38f2312886f6589
cansahinbas <jshahimbash@gmail.com>
2025-05-27 21:19:56 +0400

    second commit

---
+++ Day 32/classwork/can.py
@@ -0,0 +1,18 @@
+#1
+names = ["can", "gio", "nika"]
+num = -1
+
+while num < 0 or num > 2:
+    num = int(input("Enter a number from 0 to 2!: "))
+
+print(names[num])
+
+
+#2
+# რატომ გამოიტანს ეს კოდი შეცდომას?
+
+#word = "Python"
+#word[0] = "B"
+#print(word) 
+
+#რადგან სტრინგი არ იცვლება 
---
+++ Day 32/classwork/can2.py
@@ -0,0 +1,5 @@
+names = [1,2,3,4,5,6,7,8,9,10]
+
+print(names[2::3])
+
+print(names[3::3])
