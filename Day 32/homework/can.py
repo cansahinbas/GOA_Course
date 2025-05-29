544bfabe08b96d83d915010e0530a968ecdeb951
cansahinbas <jshahimbash@gmail.com>
2025-05-29 18:30:25 +0400

    second commit

--- Day 32/homework/can.py
+++ Day 32/homework/can.py
@@ -1,1 +1,23 @@
-#weris proceshi var
+#1
+#იგიგვე renge ფუნქცია მაგრამ გამოიყენება სიებში
+
+#2
+for i in range(2,5):
+    print(i)
+
+for i in range(2,5,3):
+    print(i)
+
+for i in range(2,15,2):
+    print(i)
+
+
+#3
+
+numbers = [1,2,3,4,5,7,8,9,10]
+
+print(numbers[1:8:3])
+
+#3 
+
+print(numbers[2::])
