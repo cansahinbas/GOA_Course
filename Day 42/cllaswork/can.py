077972b42f95a784638f3e60b0356308fbdb8cf7
cansahinbas <jshahimbash@gmail.com>
2025-07-01 22:42:49 +0400

    second commit

--- Day 42/cllaswork/can.py
+++ Day 42/cllaswork/can.py
@@ -1,1 +1,44 @@
-#არ გვქონდა საკლასო
+#1
+def even_or_odd():
+    user_input=int(input("enter numbers: "))
+    if user_input % 1 == 0:
+        print("რუსცხვი ლუწია")
+    else:
+        print("რიცხვი კენტია")
+
+even_or_odd()
+
+
+#2
+
+def name_list():
+    list=[]
+
+    for i in range(5):
+        can=input("enter name: ")
+        list.append(can)
+
+    print(list)
+
+name_list()
+
+#3
+
+def even_or_odd(numbers):
+    bla = 0
+    for i in range(1 , numbers + 1):
+        print(i) 
+
+even_or_odd(5)
+
+#4
+
+def number_bla(numbers):
+    result = 0
+    for  i in range(numbers + 1, numbers+100):
+        if i % 2 == 0:
+            result += 1
+        if result == 1:
+            print(i)
+
+number_bla(10)
--- Day 42/homework/can.py
+++ Day 42/homework/can.py
@@ -1,1 +1,49 @@
-#არ მოგვცეს დავალება
+#1
+
+#ფუნქციის შესაქმნეწლად ვიყენებთ def keyword-ს მერე ვუწერეთ ნებისმიერ სახელს და ბოლოს ფრჩხილები და ორი წერტილი
+
+#2
+
+def numbers_jami(a,b):
+    print(str(a) + " + " + str(b) + " = " + str(a+b))
+
+numbers_jami(5,7)
+numbers_jami(4,12)
+numbers_jami(9,4)
+numbers_jami(312,2311)
+
+#3
+
+def even_or_odd():
+    user_input=int(input("enter numbers: "))
+    if user_input % 1 == 0:
+        print("რუსცხვი ლუწია")
+    else:
+        print("რიცხვი კენტია")
+
+even_or_odd()
+
+#4
+
+def numbers_kvadrat(numbers):
+    print(str(numbers) + " კვადრატია = " + str(numbers*numbers))
+    
+numbers_kvadrat(3)
+numbers_kvadrat(85)
+numbers_kvadrat(7)
+numbers_kvadrat(25)
+numbers_kvadrat(12)
+
+#5
+
+def words(w):
+    print(w.upper())
+
+words("bla_blabla")
+
+#6
+
+def name_username(name,username):
+    print("youre name is so good " + name + "and not same usrname " + username)
+
+name_username("can","sahinbas")
